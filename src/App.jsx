6a68b437734bc726b570f0392e1fa525d6c5cf64
Christian Larios <christiandlf15@gmail.com>
2023-03-17 01:44:08 -0600

    Completed designs

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 // import { useState } from 'react';
 import styles from './App.module.css';
 import UserMsg from './components/user-msg';
+import pfp from './assets/user-circle.svg';
+import BotMsg from './components/bot-msg';
+
+const msg =
+	'Hello, do a task for me. I want you to go to the store and buy me some milk.';
+const response = "Sorry I cann't do that. Im just a bot.";
 
 function App() {
 	return (
@@ -13,7 +19,8 @@ function App() {
 			</nav>
 			<section className={styles.chatbox}>
 				<div className={styles['chat-log']}>
-					<UserMsg></UserMsg>
+					<UserMsg msg={msg} pfp={pfp}></UserMsg>
+					<BotMsg response={response}></BotMsg>
 				</div>
 				<div className={styles['input-zone']}>
 					<div className={styles['input-bar']}>
--- src/components/user-msg.jsx
+++ src/components/user-msg.jsx
@@ -1,7 +1,19 @@
+import DefaultAvatar from './avatar';
 import styles from './user-msg.module.css';
 
 const UserMsg = ({ msg }) => {
-	<div className={styles['msg-container']}></div>;
+	return (
+		<div className={styles['msg-area']}>
+			<div className={styles['msg-container']}>
+				<div className={styles.pfp}>
+					<DefaultAvatar></DefaultAvatar>
+				</div>
+				<div className={styles.msg}>
+					<p>{msg}</p>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default UserMsg;
