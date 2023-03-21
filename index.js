d610756529cb72671b3e52d16c5bc395181c1ec5
Christian Larios <88118236+larioscow@users.noreply.github.com>
2023-03-21 09:25:48 -0600

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-const port = 3000;
+const port = 3080;
 
 app.post("/", async (req, res) => {
   const { message } = req.body;
